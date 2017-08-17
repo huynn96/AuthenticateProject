0fd7ce521b19739f8dc04a02458222b61c80ba3d
lecongchinh <lecongchinh.ptit@gmail.com>
2017-08-17 17:07:30 +0700

    add user inerface

--- http/router.js
+++ http/router.js
@@ -1,4 +1,4 @@
-var express = require('express');
+let express = require('express');
 
 let router = express.Router();
 
@@ -9,5 +9,10 @@ router.get('/register', (req, res, next) => {
 router.get('/login', (req, res, next) => {
 	res.render('login.html');
 });
-
+router.get('/edit', (request, response, next) => {
+	response.render('edit.html');
+});
+router.get('/detail', (request, response, next) => {
+	response.render('detail.html');
+});
 module.exports = router;
